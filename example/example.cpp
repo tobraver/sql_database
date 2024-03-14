302dd68960f408e31649f6f6e650e0fda5cc97c3
wz <1208827324@qq.com>
2024-03-14 16:10:49 +0800

    change

--- example/example.cpp
+++ example/example.cpp
@@ -33,7 +33,7 @@ int main(int argc, char const *argv[])
         {
             printf("%s ", it.c_str());
         }
-    printf("\n");
+        printf("\n");
     }
     
     sql_database::sqlite::close();
